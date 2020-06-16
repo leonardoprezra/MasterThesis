e1d391d01429704b5f2c0d6af6d45da114f2bc14
Leonardo Perez <leonardo.perez@fau.de>
2020-06-16 18:49:32 +0200

    Plot only error bars and include gsd2pos.py

---
+++ Code/gsd2pos.py
@@ -0,0 +1,115 @@
+'''
+Convert all frames of the <in_file> and saves them as .pos.
+
+'''
+
+import gsd
+import gsd.hoomd
+import math
+
+import sys
+import argparse
+import os
+
+# Name of directory to store converted files
+dir_name = 'data_slice_pos/'
+
+# Command line argument parsing
+parser = argparse.ArgumentParser(
+    description='Extract the last <frame_num> of the <in_file> and saves them as .gsd and .pos.')
+parser.add_argument('-i', '--in-file', type=str, nargs='+',
+                    dest='in_file', help='input .gsd file')
+
+args = parser.parse_args()
+
+# Create directories
+try:
+    if(not os.path.exists(dir_name)):
+        os.mkdir(dir_name)
+except OSError as e:
+    if e.errno != 17:
+        raise
+    pass
+
+
+# Convert .gsd to .pos
+
+
+def convert2pos(inFN, outFN):
+    '''Convert .gds file to .pos.
+
+    Parameters
+    ----------
+    inFN : str
+        Input .gsd file.
+        outFN : str
+        Output .pos file.
+
+
+    Returns
+    -------
+    None
+    '''
+    t = gsd.hoomd.open(name=inFN, mode='rb')
+
+    print('!!!!!!!!!!!!!!!!!!!')
+    print(inFN)
+    print(inFN.split('_')[1].split('-')[1])
+
+    shape = inFN.split('_')[1].split('-')[1]
+
+    if shape != 'one':
+        # Diameter of core particles
+        for i in range(t[0].particles.N):
+            if t[0].particles.typeid[i] == 0:
+                diam_core = t[0].particles.diameter[i]
+                break
+
+        # Diameter of halo particles
+        for i in range(t[0].particles.N):
+            if t[0].particles.typeid[i] == 1:
+                diam_halo = t[0].particles.diameter[i]
+                break
+    else:
+        # Diameter of core particles
+        for i in range(t[0].particles.N):
+            if t[0].particles.typeid[i] == 0:
+                diam_core = t[0].particles.diameter[i]
+                break
+
+    with open(outFN[:-3] + 'pos', 'w') as file1:
+
+        for f in t:
+
+            box = f.configuration.box
+
+            if shape != 'one':
+                file1.write("box {:f} {:f} {:f}\n".format(
+                    box[0], box[1], box[2]))
+                file1.write(
+                    'def A "sphere {:f} FF0000"\n'.format(diam_core))  # Red
+                file1.write('def B "sphere {:f} 0000FF"\n'.format(
+                    diam_halo))  # Blue
+            else:
+                file1.write("box {:f} {:f} {:f}\n".format(
+                    box[0], box[1], box[2]))
+                file1.write(
+                    'def A "sphere {:f} FF0000"\n'.format(diam_core))  # Red
+
+            for i in range(f.particles.N):
+                if f.particles.typeid[i] == 0:
+                    file1.write("A " + "{:f} {:f} {:f}\n".format(
+                        *f.particles.position[i]))
+
+                elif f.particles.typeid[i] == 1:
+                    file1.write("B " + "{:f} {:f} {:f}\n".format(
+                        *f.particles.position[i]))
+
+            file1.write("eof\n")
+
+
+for in_file in args.in_file:
+    # Output .gsd file name
+    outFN = dir_name + in_file
+
+    convert2pos(inFN=in_file, outFN=outFN)
